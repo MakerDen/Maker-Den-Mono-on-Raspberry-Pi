b3ee684b5e3fe947defe494750cd5d93e3e5ab9c
gloveboxes <dglover.au@hotmail.com>
2015-04-08 23:54:52 +1000

    renamed matrix

--- MakerDenMono/Program.cs
+++ MakerDenMono/Program.cs
@@ -17,7 +17,7 @@ namespace MakerDenMono {
             using (SensorMemory mem = new SensorMemory(2000, "mem01"))
             using (led = new LedDigital(gpioDriver, ProcessorPin.Pin13, "led01"))
             using (Relay relay = new Relay(gpioDriver, ProcessorPin.Pin06, "relay01"))
-            using (Adafruit8x8Matrix miniMatrix = new Adafruit8x8Matrix(new Ht16K33I2cConnection(i2cDriver.Connect(0x70)), "minimatrix"))
+            using (Adafruit8x8Matrix miniMatrix = new Adafruit8x8Matrix(new Ht16K33I2cConnection(i2cDriver.Connect(0x70)), "matrix"))
             using (AdaFruitMatrixRunV2 matrix = new AdaFruitMatrixRunV2(miniMatrix))
             using (SensorLight light = new SensorLight(spiConnection, 1000, "light01"))
             using (SensorMcp9701a tempMcp9701a = new SensorMcp9701a(spiConnection, 15000, "temp02")) {
